9d6e09e35e6bc1dbac9f5d6a5d70fce8a47108d9
Williamlittlewhite <37958264+Williamlittlewhite@users.noreply.github.com>
2022-03-15 23:55:31 +0800

    Add files via upload

---
+++ PAT-basic level02/PAT-basic level02/1016.cpp
@@ -0,0 +1,30 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<cstdio>
+//#include<vector>
+//#include<string>
+//#include<algorithm>
+//#include<math.h>
+// 本题没有难度 注意如何把字符串数字转换为int  '6'-'0'=6 
+//using namespace std;
+//int FindFunction(string& a, int& b)
+//{
+//	int i = 0,sum=0,count=0;
+//	for (i = 0;i < a.size();i++)
+//	{
+//		if (a[i]-'0' == b)
+//		{
+//			sum += b*pow(10,count);
+//			count++;
+//		}
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	string A,B;
+//	int D_A, D_B;
+//	cin >> A >> D_A >> B >> D_B;
+//	cout << FindFunction(A, D_A) + FindFunction(B, D_B);
+//
+//}
